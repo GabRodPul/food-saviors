293ac6c446711b44ac8b8bed15deefdb35121a01
Corny-atk <cornelis.attakpa05@gmail.com>
2025-04-07 09:39:58 +0100

    Adding Purchase router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -8,6 +8,7 @@ import { userRouter } from "./routers/user";
 import { businessRouter } from "./routers/business";
 import { reviewRouter } from "./routers/review";
 import { allergenRouter } from "./routers/allergen";
+import { purchaseRouter } from "./routers/purchase";
 
 /**
  * This is the primary router for your server.
@@ -20,7 +21,8 @@ export const appRouter = createTRPCRouter({
   user: userRouter,
   business: businessRouter,
   review: reviewRouter,
-  allergen: allergenRouter
+  allergen: allergenRouter,
+  purchase: purchaseRouter
 });
 
 // export type definition of API
