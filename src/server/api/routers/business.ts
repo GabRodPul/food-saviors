eaba4fd252dae5530d3b32d1a99bf6fa74486603
Corny-atk <cornelis.attakpa05@gmail.com>
2025-04-10 19:33:42 +0100

    Add transaction (Business + Review count + Average rating)

--- src/server/api/routers/business.ts
+++ src/server/api/routers/business.ts
@@ -3,11 +3,7 @@ import {
   publicProcedure,
 } from "@food-saviors/server/api/trpc";
 import { IdSchema } from "@food-saviors/types/data/pkey";
-
-// TODO: Implement business router
-import {
-  BusinessSchema
-} from "@schemas/*";
+import { BusinessSchema, ReviewSchema } from "@schemas/*";
 
 export const businessRouter = createTRPCRouter({
   create: publicProcedure
@@ -25,13 +21,12 @@ export const businessRouter = createTRPCRouter({
   getAllWhere: publicProcedure
     .input(BusinessSchema.omit({ id: true }).partial())
     .query(async ({ ctx, input }) => {
-      return ctx.db.business.findMany({ where: input })
+      return ctx.db.business.findMany({ where: input });
     }),
 
-  getAll: publicProcedure
-    .query(async ({ ctx }) => {
-      return ctx.db.business.findMany();
-    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.business.findMany();
+  }),
 
   update: publicProcedure
     .input(BusinessSchema)
@@ -42,6 +37,44 @@ export const businessRouter = createTRPCRouter({
   delete: publicProcedure
     .input(IdSchema)
     .mutation(async ({ ctx, input }) => {
-      return ctx.db.business.delete({ where: { id: input.id } })
+      return ctx.db.business.delete({ where: { id: input.id } });
     }),
-})
+
+  // Mutation to add a review and aggregate statistics
+  addReview: publicProcedure
+    .input(ReviewSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.$transaction(async (tx) => {
+        const business = await tx.business.findUnique({
+          where: { id: input.businessId },
+          select: { id: true },
+        });
+        if (!business) {
+          throw new Error(`Business with id ${input.businessId} not found.`);
+        }
+
+        const newReview = await tx.review.create({
+          data: {
+            businessId: input.businessId,
+            date: input.date ?? new Date(),
+            text: input.text,
+            rating: input.rating,
+            userId: input.userId
+          },
+        });
+
+        // Calculate business statistics: number of reviews and average ratings
+        const aggregation = await tx.review.aggregate({
+          _avg: { rating: true },
+          _count: { rating: true },
+          where: { businessId: input.businessId },
+        });
+
+        return {
+          review: newReview,
+          reviewCount: aggregation._count.rating,
+          averageRating: aggregation._avg.rating,
+        };
+      });
+    }),
+});
